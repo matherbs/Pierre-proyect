d7027b2568d6918c5473c8c138ae4ee9f373df6b
matherbs <32384824+matherbs@users.noreply.github.com>
2017-11-03 16:24:33 +0100

    Initial commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<header id="main-header">
+
+    <a id="logo-header" href="#">
+      <span class="site-name">FoodForYou</span>
+    </a> <!-- / #logo-header -->
+    <nav>
+      <ul>
+        <li><a href="#">Health</a></li>
+        <li><a href="#">Raw Food</a></li>
+        <li><a href="#">Recipes</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">LOLOLO</a></li>
+      </ul>
+    </nav>
+  <!-- / nav -->
+
+  </header>
---
+++ navigation.php
@@ -0,0 +1,31 @@
+<div class="menu">
+  <ul>
+    <li>
+      <a href="#">Miau</a>
+    </li>
+    <li>
+      <a href="#">Item 2</a>
+    </li>
+    <li>
+      <a href="#">Item 3</a>
+    </li>
+    <li>
+      <a href="#">Item 4</a>
+    </li>
+    <li>
+      <a href="#">Item 5</a>
+    </li>
+    <li>
+      <a href="#">Item 6</a>
+    </li>
+    <li>
+      <a href="#">Item 7</a>
+    </li>
+    <li>
+      <a href="">Item 8</a>
+    </li>
+    <li>
+      <a href="">Item 9</a>
+    </li>
+  </ul>
+</div>
